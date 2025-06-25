193a7de29a4339271a8187d2bf5899a7f62a48cd
reilly-23615971 <23615971@student.uwa.edu.au>
2025-06-25 21:37:24 +0800

    Corrected schema issues; removed files from inner folder; added example config file

--- serverApp.py
+++ serverApp.py
@@ -29,6 +29,8 @@ async def runModel(config: modelGuideFile):
     modelConfiguration = config.model_dump(exclude_unset = True)
     print(f'Simulation received; name = {modelConfiguration["name"]}')
     # TODO: run flusim simulation and analysis tools
+    # Specify exact filenames instead of using --set and --community to 
+    # avoid issues with improper parameter identification
     time.sleep(3) # Simulate long running time
     print('Simulation complete, sending response')
     # For now this'll just send over the existing CSV file
