72fca4f6466fe483b9510f301cdff67134a01d52
reilly-23615971 <23615971@student.uwa.edu.au>
2025-08-25 20:40:43 +0800

    Now actually runs files, uses client request to determine how to use analysis tools, and generates data to send back to the client (possibly multiple files); modified file format

--- serverApp.py
+++ serverApp.py
@@ -3,23 +3,43 @@
 
 # Imports
 import time
+import os
+import sys
 import json
+import zipfile
 import logging
-from fastapi import FastAPI
+from fastapi import FastAPI, BackgroundTasks
 from fastapi.responses import FileResponse
 from fastapi.middleware.cors import CORSMiddleware
 
-from ModelSchema import modelGuideFile
+from serverFiles.ModelSchema import modelGuideFile
+from serverFiles.simulationFunctions import (
+    runSimulation, generateToolboxConfig, epidemic, asir, simLocation
+)
 
+# Ensure Flusim imports work properly when called outside of toolbox
+sys.path.append(os.path.join(os.getcwd(), simLocation, 'src/toolbox'))
+from analysis.AnalysisStat import AnalysisStat
 
 
 # Logging config
 logging.basicConfig(
-    filename = './serverAppLogs.txt', filemode = 'a', 
+    filename = 'serverFiles/serverAppLogs.txt', filemode = 'a', 
     format = '%(asctime)s,%(msecs)03d %(name)s %(levelname)s %(message)s', 
-    datefmt = '%Y-%m-%d %H:%M:%S', level = logging.INFO
+    datefmt = '%Y-%m-%d %H:%M:%S', level = logging.DEBUG
 )
 
+# Throw error if Flusim files aren't present
+if not os.path.isfile('/src/toolbox/toolbox.py'): raise FileNotFoundError((
+    'Flusim files not found. Ensure that this application is '
+    'present in the same directory as the Flusim simulation files.'
+))
+
+# Function to remove excess files
+def clearFiles(files): 
+    print(f'[clearFiles] Deleting the following files for cleanup purposes:')
+    for f in files: print('   ', f)
+    for f in files: os.remove(f)
 
 
 # Define main Flusim app
@@ -36,12 +56,72 @@ flusimApp.add_middleware(
 
 # Define route for receiving model parameters
 @flusimApp.post('/runModel')
-async def runModel(config: modelGuideFile):
-    modelConfiguration = config.model_dump(exclude_unset = True)
-    print(f'Simulation received; name = {modelConfiguration["name"]}')
-    # TODO: run flusim simulation and analysis tools
-    # Ensure middle joint is consistent with toolbox_config.json
-    time.sleep(3) # Simulate long running time
-    print('Simulation complete, sending response')
-    # For now this'll just send over the existing CSV file
-    return FileResponse('./newcastle-coronaV96-epidemic-mean.csv')
+async def runModel(config: modelGuideFile, cleanup: BackgroundTasks):
+    # Get relevant attributes from config file
+    sessionID = config.description
+    community = config.community_used[0]
+    requiredData = config.middle_joint
+    #simCount = len(config.simulation_sets[0].simulations)
+    print(f'Simulation received; name = {config.name}, ID = {sessionID}')
+
+    # Generate toolbox config file
+    toolboxPath = generateToolboxConfig(sessionID, requiredData)
+
+    # Run the Flusim simulation
+    simFiles = runSimulation(config)
+    print('Simulation complete\n')
+    
+    returnFiles = []
+    # Use middle joint to determine analyses to run
+    if requiredData:
+        # Epidemic tool
+        if 'usingEpidemic' in requiredData: 
+            sumStat = (
+                AnalysisStat.MEAN if 'EMean' in requiredData 
+                else AnalysisStat.MEDIAN
+            )
+            epiCumulative = 'ECumulative' in requiredData
+            epiAge = 'EAgeBased' in requiredData
+            returnFiles.append(epidemic(
+                community, requiredData, sessionID, summaryStat = sumStat, 
+                cumulative = epiCumulative, byAge = epiAge
+            ))
+        # Asir tool
+        if 'usingAsir' in requiredData:
+            sumStat = (
+                AnalysisStat.MEAN if 'AMean' in requiredData 
+                else AnalysisStat.MEDIAN
+            )
+            asirProportion = 'AProportion' in requiredData
+            asirIndigenous = 'AIndigenous' in requiredData
+            asirPregnant = 'APregnant' in requiredData
+            asirVaccinated = 'AVaccinated' in requiredData
+            returnFiles.append(asir(
+                community, requiredData, sessionID, 
+                summaryStat = sumStat, getProportion = asirProportion, 
+                onlyIndigenous = asirIndigenous, onlyPregnant = asirPregnant, 
+                onlyVaccinated = asirVaccinated
+            ))
+    # If all else fails, run epidemic
+    if not returnFiles: 
+        print('No analyses specified; defaulting to epidemic')
+        returnFiles.append(epidemic(community, requiredData, sessionID))
+
+    print(f'\nAnalysis files:')
+    for file in returnFiles: print('   ', file)
+
+    # Zip together the analysis files if necessary
+    if len(returnFiles) != 1:
+        zipPath = f'serverFiles/{id}_analysis.zip'
+        with zipfile.ZipFile(zipPath, mode = 'w') as analysis:
+            for file in returnFiles: analysis.write(file)
+        returnFiles.append(zipPath)
+        finalPath = zipPath
+    # Just return lone CSV if only one analysis needed
+    else: finalPath = returnFiles[0]
+    
+    # Schedule files created here to be removed
+    cleanup.add_task(clearFiles, simFiles + returnFiles + [toolboxPath])
+
+    print(f'\nSimulation for session {sessionID} complete, returning data')
+    return FileResponse(finalPath)
---
+++ serverFiles/simulationFunctions.py
@@ -0,0 +1,168 @@
+# Flusim Web Interface Application
+# Developed by Reilly Evans
+# Functions for running the Flusim simulation
+
+# Note that this file assumes it's just outside the smrg-flusim folder
+
+# Imports
+import time
+import datetime
+import os
+import sys
+from argparse import Namespace
+import json
+import logging
+from fastapi import FastAPI
+from fastapi.responses import FileResponse
+from fastapi.middleware.cors import CORSMiddleware
+from ModelSchema import modelGuideFile
+
+# Change this if file location relative to Flusim changes
+simLocation = ''
+
+# Ensure Flusim imports work properly when called outside of toolbox
+sys.path.append(os.path.join(os.getcwd(), simLocation, 'src/toolbox'))
+
+# Flusim Simulation Imports
+from ToolboxConfiguration import ToolboxConfiguration
+from commands.Run.RunCommand import RunCommand
+from commands.epidemic import EpidemicCurveCommand
+from commands.ASIRAnalysis import AsirCommand
+from analysis.AnalysisStat import AnalysisStat
+
+# Logging
+functionLog = logging.getLogger(__name__)
+
+# Define config early
+toolboxConfig = ToolboxConfiguration('conf/toolbox_config.json')
+
+
+
+
+
+# Function to generate the required toolbox config file
+def generateToolboxConfig(id, joint):
+    toolboxConfigSettings = {
+        "simulator_executable": "src/simulator/build/src/simulator",
+        "baseline_file": "conf/simulation.baseline.json",
+        "middle_joint": joint,
+        "set_id_joint": "-",
+        "event_suffix": ".sqlite",
+        "input_directory": "results/",
+        "output_directory": "post-analysis",
+        "communities": [
+            {
+                "name": "newcastle",
+                "path": "communities/newcastle2011"
+            },
+            {
+                "name": "cairns",
+                "path": "communities/cairns2011"
+            },
+            {
+                "name": "integration",
+                "path": "integration/community"
+            }
+        ]
+    }
+    toolboxConfigPath = f'serverFiles/toolbox_config_{id}.json'
+    print((
+        '[generateToolboxConfig] Generating toolbox '
+        f'configuration file {toolboxConfigPath}'
+    ))
+    with open(toolboxConfigPath, 'w') as file: json.dump(
+        toolboxConfigSettings, file
+    )
+    return toolboxConfigPath
+
+
+# Function to run the simulation using the given config files
+def runSimulation(configData: modelGuideFile):
+    # Save guide file to JSON
+    sessionID = configData.description
+    guidePath = f'serverFiles/flusim-{sessionID}.guide.json'
+    with open(guidePath, 'w') as file: file.write(
+        configData.model_dump_json(indent = 4, exclude_unset = True)
+    )
+    
+    # Run the simulation
+    RunCommand().run_command(Namespace(guide = guidePath), toolboxConfig)
+
+    """# Modified toolbox function code for running the model
+    startTime = time.monotonic()
+
+    # C++ code is multithreaded, so we don't need extra threading here
+    # TODO: Use toolbox config with updated middle joint, if necessary
+    queueBuilder = ScenarioBuilder(toolboxConfig, guidePath)
+    for scenario in queueBuilder.generateScenarios(): scenario.run()
+
+    duration = time.monotonic() - startTime
+    formattedDuration = str(datetime.timedelta(seconds = round(duration)))
+    functionLog.info(
+        f'[runSimulation] All simulations completed in {formattedDuration}'
+    )"""
+
+    # Get list of sim files so they can be deleted once the 
+    # simulation and analysis are complete
+    simFiles = [
+        os.path.join(simLocation, 'results', ((
+            f'{configData.community_used[0]}{configData.middle_joint}'
+            f'{configData.simulation_sets[0].version}-0{i + 1}.sqlite'
+        ))) 
+        for i in range(len(configData.simulation_sets[0].simulations))
+    ]
+
+    # Return required files so they can be deleted later
+    return simFiles + [guidePath]
+
+
+
+# Function for epidemic toolbox function
+def epidemic(
+    communityName, joint, id, summaryStat = AnalysisStat.MEDIAN, 
+    cumulative = False, byAge = False
+):
+    # Run epidemic analysis
+    epidemicArgs = Namespace(
+        community = communityName, set = id, 
+        calculate_stat = summaryStat, 
+        cumulative_sum = cumulative, by_age = byAge
+    )
+    print(
+        f'[epidemic] Running "epidemic" analysis for set {id} [{summaryStat}]', 
+        '[cumulative]' if cumulative else '[individual]', 
+        '[age-separated]' if byAge else '[age-combined]'
+    )
+    EpidemicCurveCommand().run_command(epidemicArgs, toolboxConfig)
+
+    # Return the name of the newly processed file
+    filename = ((
+        f'{communityName}{joint}-epidemic-'
+        f'{'cumulative-' if cumulative else ''}{summaryStat}.csv'
+    ))
+    return os.path.join(simLocation, f'post-analysis/{filename}')
+
+# Function for asir toolbox function
+def asir(
+    communityName, joint, id, summaryStat = AnalysisStat.MEDIAN, 
+    getProportion = False, onlyIndigenous = False, onlyPregnant = False, 
+    onlyVaccinated = False
+):
+    # Run epidemic analysis
+    epidemicArgs = Namespace(
+        community = communityName, set = id, calculate_stat = summaryStat, 
+        proportion = getProportion, indigenous = onlyIndigenous, 
+        pregnant = onlyPregnant, vaccinated = onlyVaccinated
+    )
+    print(
+        f'[asir] Running "asir" analysis for set {id} [{summaryStat}]', 
+        '[proportionate]' if getProportion else '[discrete]', 
+        '[indigenous only]' if onlyIndigenous else '[all demographics]', 
+        '[pregnant only]' if onlyIndigenous else '[all pregnant status]', 
+        '[vaccinated only]' if onlyIndigenous else '[all vaccine status]', 
+    )
+    AsirCommand().run_command(epidemicArgs, toolboxConfig)
+
+    # Return the name of the newly processed file
+    filename = f'{communityName}{joint}-asir-{summaryStat}.csv'
+    return os.path.join(simLocation, f'post-analysis/{filename}')
